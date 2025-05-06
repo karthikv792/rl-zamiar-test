8ae145b8b436eac55cb68f0010be559d23c95c14
Durgesh-Kalwar <dghkalwar007@gmail.com>
2025-05-05 21:40:47 -0700

    modified countdown.py file

--- examples/data_preprocess/countdown.py
+++ examples/data_preprocess/countdown.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
     parser.add_argument('--max_target', type=int, default=1000)
     parser.add_argument('--min_number', type=int, default=1)
     parser.add_argument('--max_number', type=int, default=100)
-    parser.add_argument('--train_size', type=int, default=102400) #327680
+    parser.add_argument('--train_size', type=int, default=7936) #327680
     parser.add_argument('--test_size', type=int, default=1024)
     parser.add_argument('--template_type', type=str, default='base') #base
     parser.add_argument('--data_source', type=str, default='countdown')
